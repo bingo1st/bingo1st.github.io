65708c9efec5a6c05fd5ae3659acbfd2fd85af52
Apollo <youbing.song@gmail.com>
2015-03-11 22:26:31 +0800

    Improve efficiency

--- js/app.js
+++ js/app.js
@@ -1,1 +1,447 @@
-alert(0);
+    var config = [
+        {
+            "question": "楼梯陡、门口窄，太容易发生踩踏事故找谁？",
+            "options": ["规划局", "日料店", "安监局"],
+            "bg": '#799475', 
+            "elements": 
+            [
+                {
+                    "src":"1_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"1_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "rubberBand",
+                    "duration": 1,
+                    "delay": .1
+                },
+                {
+                    "src":"1_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "wobble",
+                    "duration": .7,
+                    "delay": .5
+                }
+            ]
+        },
+        {
+            "question": "圈地私建堵了胡同口，咋弄？",
+            "options": ["规划局", "城管", "土行孙"],
+            "bg":'#49675c',
+            "elements": 
+            [
+                {
+                    "src":"2_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"2_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "rubberBand",
+                    "duration": 1,
+                    "delay": .8
+                }
+            ]
+        },
+        {
+            "question": "隔壁的仓库乱扯电线，有火灾隐患！",
+            "options": ["蝙蝠侠", "安监局", "119"],
+            "bg": '#3c7985',
+            "elements": 
+            [
+                {
+                    "src":"3_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"3_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "flash",
+                    "duration": 1,
+                    "delay": .9
+                }
+            ]
+        },
+        {
+            "question": "邻居的破猫成宿叫春，闹挺！",
+            "options": ["社区", "人口计生委", "黑猫警长"],
+            "bg": '#54b4c1',
+            "elements": 
+            [
+                {
+                    "src":"4_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"4_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "flash",
+                    "duration": 1,
+                    "delay": .8
+                },
+                {
+                    "src":"4_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "rubberBand",
+                    "duration": .7,
+                    "delay": 1.1
+                }
+            ]
+        },
+        {
+            "question": "井盖不翼而飞，谁管？",
+            "options": ["忍者神龟", "产权单位", "城管"],
+            "bg": '#ffd591',
+            "elements": 
+            [
+                {
+                    "src":"5_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"5_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "flash",
+                    "duration": 1,
+                    "delay": .8
+                },
+                {
+                    "src":"5_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "rubberBand",
+                    "duration": .7,
+                    "delay": 1.5
+                }
+            ]
+        },
+        {
+            "question": "没有暖气，也要倔强地活到春天！",
+            "options": ["质监局", "供暖办", "王思聪"],
+            "bg": '#77bc72',
+            "elements": 
+            [
+                {
+                    "src":"6_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"6_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "shake",
+                    "duration": .7,
+                    "delay": .5
+                },
+                {
+                    "src":"6_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "fadeIn",
+                    "duration": .7,
+                    "delay": 1.5
+                }
+            ]
+        },
+        {
+            "question": "狗狗的厕所都不放过，无耻！",
+            "options": ["光头强", "环保局", "园林绿化局"],
+            "bg": '#956d5c',
+            "elements": 
+            [
+                {
+                    "src":"7_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"7_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "rubberBand",
+                    "duration": .7,
+                    "delay": .5
+                },
+                {
+                    "src":"7_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "tada",
+                    "duration": .7,
+                    "delay": 1.8
+                }
+            ]
+        },
+        {
+            "question": "到药店买到假药啦！",
+            "options": ["德云社", "工商局", "食药监局"],
+            "bg": '#4d628a', 
+            "elements": 
+            [
+                {
+                    "src":"8_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"8_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "slideInLeft",
+                    "duration": .7,
+                    "delay": .5
+                },
+                {
+                    "src":"8_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "flash",
+                    "duration": .7,
+                    "delay": 1.5
+                }
+            ]
+        },
+        {
+            "question": "老婆我错了，别动刀，救命啊！",
+            "options": ["社区", "110", "神盾局"],
+            "bg": '#d78152', 
+            "elements": 
+            [
+                {
+                    "src":"9_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"9_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "slideInRight",
+                    "duration": .7,
+                    "delay": .5
+                },
+                {
+                    "src":"9_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "shake",
+                    "duration": .7,
+                    "delay": 1.6
+                }
+            ]
+        },
+        {
+            "question": "养老保险查询应该问谁？",
+            "options": ["社保中心", "邻居王大妈", "民政局"],
+            "bg": '#5e996a', 
+            "elements": 
+            [
+                {
+                    "src":"10_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"10_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "rubberBand",
+                    "duration": .7,
+                    "delay": .5
+                },
+                {
+                    "src":"10_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "zoomInDown",
+                    "duration": .7,
+                    "delay": 1.2
+                }
+            ]
+        },
+        {
+            "question": "   路边乱停车谁管？",
+            "options": ["李刚", "车管所", "交通支队"],
+            "bg": '#a5b6a5',
+            "elements": 
+            [
+                {
+                    "src":"11_0.png",
+                    "left": "0",
+                    "top": "0"
+                },
+                {
+                    "src":"11_1.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "slideInRight",
+                    "duration": .7,
+                    "delay": .5
+                },
+                {
+                    "src":"11_2.png",
+                    "left": "0",
+                    "top": "0",
+                    "animation": "flash",
+                    "duration": .7,
+                    "delay": 1.3
+                }
+            ]
+        }
+    ];
+
+    
+    $(document).ready(function() {
+        var _cover = $(".cover");
+            _headPhone = $(".touchstyle-headphone");
+            _music = document.getElementById("music");
+
+        _cover.on("touchstart", function(){
+            _cover.hide();
+            setTimeout(function(){
+                $(".section1 .animation-mark").addClass("animated");
+                _headPhone.addClass("play")
+                _music.setAttribute("src", "images/pics/paopaotang.mp3");
+                _music.play();
+            }, 200);
+        });
+
+        setTimeout(function(){
+        	var _questionStore = randomArray(config, 8);
+            initPages(_questionStore);
+        }, 1000);
+
+        _headPhone.click(function(){
+            _headPhone.hasClass("play")? _music.pause() : _music.play();
+            _headPhone.toggleClass("play");
+        });
+    });
+
+    function randomArray(array, amount){
+    	var result = [];
+    	for (var i = amount - 1; i >= 0; i--) {
+		    //从原数组中随机取一个元素出来
+		    var index = Math.floor(Math.random() * array.length);
+		    //压入结果数组
+		    result.push(array[index]);
+		    //将该元素从原数组中删除
+		    array.splice(index, 1);
+		};
+		return result;
+    }
+
+    function nextQuestion(){
+        setTimeout(function(){
+            $.fn.fullpage.moveSectionDown();
+        }, 300);
+    }
+	
+    function initPages(questions){
+        var _html     = "";
+        var _content  = "";
+        var _i        = 0;
+        var _item     = null;
+        var _options  = null;
+        var _elements = null;
+        var _pics     = "";
+        var _container = $('#fullpage');
+
+        while(questions[_i]){
+            _item = questions[_i];
+            _pics = "";
+            _content = "";
+
+            if(_item.question && _item.options){
+                _options = _item.options;
+                _content = "<div class='question-title'>" + _item.question + "</div>";
+                $.each(_options, function(_j) {     
+                     _content += "<input type='radio' name='options_" + _i + "' id='options_" + _i + "_" + _j +"' /><label for='options_" + _i + "_" + _j +"'>" + _options[_j] + "</label>"; 
+                }); 
+            }
+            if(_item.elements){
+                _elements = _item.elements;
+                $.each(_elements, function(_j) {
+                    _elements[_j].zIndex = (_elements[_j].zIndex ? _elements[_j].zIndex : 0);
+                    _elements[_j].width = (_elements[_j].width ? _elements[_j].width : "100%");
+                    _elements[_j].top = (_elements[_j].top ? _elements[_j].top : "auto");
+                    _elements[_j].left = (_elements[_j].left ? _elements[_j].left : "auto");
+                    _elements[_j].right = (_elements[_j].right ? _elements[_j].right : "auto");
+                    _elements[_j].bottom = (_elements[_j].bottom ? _elements[_j].bottom : "auto");
+                    _elements[_j].delay = (_elements[_j].delay ? _elements[_j].delay : "0");
+                    _elements[_j].opacity = (_elements[_j].delay*1 ? "0" : "1");
+
+
+                    _pics += "<img style='animation-duration: " + _elements[_j].duration + 
+                            "s;-webkit-animation-duration:" + _elements[_j].duration +
+                            "s;animation-delay:" + _elements[_j].delay +
+                            "s;-webkit-animation-delay:" + _elements[_j].delay + 
+                            "s;top:"  + _elements[_j].top + 
+                            ";opacity:" + _elements[_j].opacity + 
+                            ";left:" + _elements[_j].left + 
+                            ";right:" + _elements[_j].right + 
+                            ";bottom:" + _elements[_j].bottom + 
+                            ";z-index:" + _elements[_j].zIndex + 
+                            ";width:" + _elements[_j].width + 
+                            ";' src='images/pics/" + _elements[_j].src + 
+                            "' class='animation-mark " + _elements[_j].animation + "'/>";
+                });
+                _pics += '<img class="question-bg animation-mark slideInUp" src="images/pics/bg_questions_' + (_i + 1) + '.png">'
+            }
+
+            _html += "<div class='section section" + (_i + 1) + "'><div class='question animation-mark slideInUp'>"+
+                        _content + '</div>' +
+                        _pics +
+                     "</div>";
+            _i++;
+        }
+
+        _container.html(_html + "<div class='section section" + (_i + 1) + "'><div class='result'></div></div>");
+
+        var _colorList = [];
+        for(var i = 0; i < questions.length; i++){
+        	_colorList[i] = questions[i].bg;
+        }
+
+        _container.fullpage({
+            navigation: false,
+            /*navigationPosition: 'right',*/
+            css3: true,
+            scrollingSpeed: 300,
+            /*easingcss3: "cubic-bezier(0.175, 0.885, 0.420, 1.310)",*/
+            /*scrollOverflow: true,*/
+            /*touchSensitivity: 2,*/
+            /*controlArrows: true,*/
+            aotoScrolling: false,
+            verticalCentered: true,
+            /*resize : true,*/
+            sectionsColor : _colorList,
+            /*normalScrollElementTouchThreshold: 5,*/
+            afterRender: function(){
+                setTimeout(function(){$('#fullpage,#fp-nav').fadeIn();},100);
+                $(".question input").click(nextQuestion);
+            },
+            afterLoad: function(anchorLink, index){
+                $(".section" + index + " .animation-mark").addClass("animated");
+            },
+            onLeave: function(index, nextIndex, direction){
+                $(".animation-mark").removeClass("animated");
+                
+            }
+        });
+
+        $.fn.fullpage.setAllowScrolling(false);
+    }
